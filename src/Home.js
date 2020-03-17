893988beabb60f6cf10f6b51619d940968e7a002
Melissa Bykersma <melissabykersma@gmail.com>
2020-03-17 16:14:35 +1100

    styled Home page

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,52 @@
 import React from "react"
+import styled from "@emotion/styled"
+import About from "./About"
+
+const HomeContainer = styled.div`
+    background: linear-gradient(rgba(45, 51, 52, 0.6), rgba(45, 51, 52, 0.6)),
+        url("../images/code-background.jpg");
+    height: 80vh;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+`
+
+const HomeContent = styled.div`
+    width: 50%;
+    margin: 24px;
+    font-family: "Lato";
+`
+
+const Heading = styled.div`
+    color: #fa8c83;
+    font-size: 36px;
+`
+
+const SubHeading = styled.div`
+    color: white;
+    font-size: 24px;
+`
 
 function Home() {
-    return <h1>Home</h1>
+    return (
+        <>
+            <HomeContainer>
+                <HomeContent>
+                    <Heading>
+                        Hello, My name is Melissa Bykersma and I'm a Junior
+                        Software Developer.
+                    </Heading>
+                    <br />
+                    <SubHeading>
+                        You can check out my projects I've completed so far in
+                        the project tab! Feel free to contact me via the online
+                        form, email or phone.
+                    </SubHeading>
+                </HomeContent>
+            </HomeContainer>
+            <About />
+        </>
+    )
 }
 
 export default Home
