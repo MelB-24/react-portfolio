f72a5229fcb26562f6d09933114a14f375a8b4a9
Melissa Bykersma <melissabykersma@gmail.com>
2020-03-24 12:01:04 +1100

    added redux packages, starting styling of projects

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,23 @@
 import React from "react"
+import styled from "styled-components"
+
+const ProjectWrapper = styled.div`
+    height: 100vh;
+    background: #fa8c83;
+`
+
+const Heading = styled.h1`
+    font-family: "Lato";
+    margin: 0;
+    padding: 36px;
+`
 
 function Projects() {
-    return <div id="projects">Projects</div>
+    return (
+        <ProjectWrapper id="projects">
+            <Heading>Projects</Heading>
+        </ProjectWrapper>
+    )
 }
 
 export default Projects
