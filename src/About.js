286ed797dd8d59a531238ffddb20b294631f85c6
Melissa Bykersma <melissabykersma@gmail.com>
2020-04-02 15:25:46 +1100

    Added svg image and styled About Page

--- src/About.js
+++ src/About.js
@@ -1,32 +1,91 @@
 import React from "react"
 import styled, { keyframes } from "styled-components"
+import image from "./about-illustration.svg"
 // import { fadeInRight } from "react-animations"
 
 // const fadeInRightAnimation = keyframes`${fadeInRight}`
 
 const AboutWrapper = styled.div`
-    height: 100vh;
-    border-bottom: solid #fa8c83 6px;
+    height: 90vh;
+    font-family: "Lato";
+    color: #2d3334;
+    padding: 36px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const AboutTextWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 900px;
 `
 
 const AboutHeading = styled.h1`
     margin: 0px;
     padding: 0px;
+    font-size: 36px;
 `
 
 const ResumeDownloadLink = styled.a`
     text-decoration: none;
+    color: #2d3334;
+    cursor: pointer;
+
+    &:hover {
+        color: #99edcc;
+    }
+`
+
+const AboutText = styled.p`
+    font-size: 20px;
+`
+
+const StyledImage = styled.img`
+    height: 200px;
+    float: right;
+    position: relative;
+    top: -150px;
 `
 
 function About() {
     return (
         <AboutWrapper id="about">
-            <AboutHeading>About</AboutHeading>
-            Click{" "}
-            <ResumeDownloadLink href="./Resume.pdf" target="_blank">
-                here
-            </ResumeDownloadLink>{" "}
-            to download my resume
+            <AboutTextWrapper>
+                <AboutHeading>About</AboutHeading>
+                <AboutText>
+                    After recently completing Coder Academy's 6 months bootcamp,
+                    I have found a real passion in Software Development.
+                    <br />
+                    <br />
+                    I am currently working on side projects but I am eager to
+                    start my career and be immersed in the industry.
+                    <br />
+                    <br />
+                    I am an easy going, confident person who loves to take on a
+                    challenge.
+                    <br />
+                    My hobbies include drinking a lot of coffee, hanging out
+                    with my two dogs and, without sounding too cliche, coding.
+                    <br />
+                    <br />
+                    Tech Stack:
+                    <ul>
+                        <li>JavaScript</li>
+                        <li>React</li>
+                        <li>Redux</li>
+                        <li>Ruby on Rails</li>
+                    </ul>
+                    <br />
+                    Click{" "}
+                    <ResumeDownloadLink href="./Resume.pdf" target="_blank">
+                        here
+                    </ResumeDownloadLink>{" "}
+                    to download my resume
+                    <StyledImage src={image} />
+                </AboutText>
+            </AboutTextWrapper>
         </AboutWrapper>
     )
 }
