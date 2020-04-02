9bbf0b94be706d5106c1b400260c5a912060ee72
Melissa Bykersma <melissabykersma@gmail.com>
2020-04-02 18:54:18 +1100

    Started styling contact me form

--- src/ContactMe.js
+++ src/ContactMe.js
@@ -3,10 +3,49 @@ import styled from "styled-components"
 
 const ContactMeWrapper = styled.div`
     height: calc(100vh - 95px);
+    background: #2d3334;
+    display: flex;
+    justify-content: center;
 `
 
+const ContentWrapper = styled.div`
+    width: 900px;
+    margin: 36px;
+    border: solid red;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const ContactMeHeader = styled.h1`
+    margin: 0;
+    color: white;
+    font-family: "Lato";
+    padding: 36px;
+`
+
+const FormWrapper = styled.form``
+
 function ContactMe() {
-    return <ContactMeWrapper id="contact">ContactMe</ContactMeWrapper>
+    return (
+        <ContactMeWrapper id="contact">
+            <ContentWrapper>
+                <ContactMeHeader>Contact Me</ContactMeHeader>
+                <FormWrapper method="POST">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" />
+
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" />
+
+                    <label htmlFor="message">Message</label>
+                    <textarea name="message" rows="3"></textarea>
+
+                    <input type="submit" />
+                </FormWrapper>
+            </ContentWrapper>
+        </ContactMeWrapper>
+    )
 }
 
 export default ContactMe
