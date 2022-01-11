0b765a15247ef87d357680ef0139c3bd76e5cce2
Melissa Bykersma <melissabykersma@gmail.com>
2022-01-11 16:11:45 +1100

    Feat: update text and update to function component

--- src/Home.js
+++ src/Home.js
@@ -4,64 +4,62 @@ import styled from "@emotion/styled"
 import { device } from "./deviceSizes"
 
 const HomeContainer = styled.div`
-    background: linear-gradient(rgba(45, 51, 52, 0.6), rgba(45, 51, 52, 0.6)),
-        url("../images/code-background.jpg");
-    height: 100vh;
-    background-size: cover;
-    display: flex;
-    align-items: center;
-    border-bottom: solid #fa8c83 6px;
+	background: linear-gradient(rgba(45, 51, 52, 0.6), rgba(45, 51, 52, 0.6)),
+		url("../images/code-background.jpg");
+	height: 100vh;
+	background-size: cover;
+	display: flex;
+	align-items: center;
+	border-bottom: solid #fa8c83 6px;
 
-    @media ${device.mobileS} {
-        height: 100vh;
-    }
+	@media ${device.mobileS} {
+		height: 100vh;
+	}
 `
 
 const HomeContent = styled.div`
-    width: 50%;
-    margin: 24px;
-    font-family: "Lato";
+	width: 50%;
+	margin: 24px;
+	font-family: "Lato";
 
-    @media ${device.tablet} {
-        width: 90%;
-    }
+	@media ${device.tablet} {
+		width: 90%;
+	}
 `
 
 const Heading = styled.div`
-    color: #fa8c83;
-    font-size: 36px;
+	color: #fa8c83;
+	font-size: 36px;
 
-    @media ${device.mobileS} {
-        font-size: 30px;
-    }
+	@media ${device.mobileS} {
+		font-size: 30px;
+	}
 `
 
 const SubHeading = styled.div`
-    color: white;
-    font-size: 24px;
+	color: white;
+	font-size: 24px;
 
-    @media ${device.mobileS} {
-        font-size: 20px;
-    }
+	@media ${device.mobileS} {
+		font-size: 20px;
+	}
 `
 
-function Home() {
-    return (
-        <HomeContainer id="home">
-            <HomeContent>
-                <Heading>
-                    Hello, My name is Melissa Bykersma and I'm a Junior Software
-                    Developer.
-                </Heading>
-                <br />
-                <SubHeading>
-                    You can check out my projects I've completed so far below!
-                    Feel free to contact me via the online form, email or
-                    LinkedIn.
-                </SubHeading>
-            </HomeContent>
-        </HomeContainer>
-    )
+const Home = () => {
+	return (
+		<HomeContainer id="home">
+			<HomeContent>
+				<Heading>
+					Hello, My name is Melissa Bykersma and I'm a Software Developer.
+				</Heading>
+				<br />
+				<SubHeading>
+					Feel free to contact me via the online form, email or
+					LinkedIn.
+				</SubHeading>
+			</HomeContent>
+		</HomeContainer>
+	)
 }
 
 export default Home
