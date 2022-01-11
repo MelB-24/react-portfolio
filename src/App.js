f355e88f7cb95b9bb92f481565595b0d7915001d
Melissa Bykersma <melissabykersma@gmail.com>
2022-01-11 16:10:04 +1100

    Project: remove Projects from prtfolio and update tabs

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import React from "react"
 
 import About from "./About"
 import Home from "./Home"
-import ProjectsContainer from "./ProjectsContainer"
 import ContactMeContainer from "./ContactMeContainer"
 import NavBar from "./common/NavBar"
 
-function App() {
-    return (
-        <>
-            <NavBar />
-            <Home id="home" />
-            <About id="about" />
-            <ProjectsContainer id="projects" />
-            <ContactMeContainer id="contact" />
-        </>
-    )
+const App = () => {
+	return (
+		<>
+			<NavBar />
+			<Home id="home" />
+			<About id="about" />
+			<ContactMeContainer id="contact" />
+		</>
+	)
 }
 
 export default App
