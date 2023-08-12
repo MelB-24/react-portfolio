b415b609fac55eb852586d1beab79cb8417f38dd
Melissa Bykersma <melissa@cognativ.com.au>
2023-08-12 19:31:32 +1000

    Project: add NumberInput component files

---
+++ src/components/NumberInput/NumberInput.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NumberInput: React.FC<NumberInputProps> = () => {
+  console.log('number input component');
+
+  return (
+    <>hi</>
+  );
+};
+
+interface NumberInputProps { }
+
+export default NumberInput;
