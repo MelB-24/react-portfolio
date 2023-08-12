0013f1ede70c2fa3314dc1e0de26f54d2b841623
Melissa Bykersma <melissa@cognativ.com.au>
2023-08-12 19:30:06 +1000

    Feat: add test for calculate and display correct balance

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import DepositCalculator from './components/DepositCalculator';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Term Deposit Calculator', () => {
+  test('correctly calculates and displays the final balance', () => {
+    const startingAmount = 10000;
+    const interestRatePercent = 1.1;
+    const investmentTermMonths = 24;
+    const interestPaid = 'month';
+    const finalBalance = 10222;
+
+    render(
+      <DepositCalculator
+        startingAmount={startingAmount}
+        interestRatePercent={interestRatePercent}
+        investmentTermMonths={investmentTermMonths}
+        interestPaid={interestPaid}
+      />,
+    );
+
+    const finalBalanceElement = screen.getByTestId('final-balance');
+    expect(finalBalanceElement).toHaveTextContent(finalBalance.toString());
+  });
 });
