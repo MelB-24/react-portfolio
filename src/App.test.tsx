5697fcb73f975cbc9dd65c48c8acbff4e2d25825
Melissa Bykersma <melissa@cognativ.com.au>
2023-08-12 19:39:49 +1000

    Fix: update test to not pass props for input data

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,21 +2,16 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import DepositCalculator from './components/DepositCalculator';
 
+const startingAmount = 10000;
+const interestRatePercent = 1.1;
+const investmentTermMonths = 24;
+const interestPaid = 'month';
+const finalBalance = 10222;
+
 describe('Term Deposit Calculator', () => {
   test('correctly calculates and displays the final balance', () => {
-    const startingAmount = 10000;
-    const interestRatePercent = 1.1;
-    const investmentTermMonths = 24;
-    const interestPaid = 'month';
-    const finalBalance = 10222;
-
     render(
-      <DepositCalculator
-        startingAmount={startingAmount}
-        interestRatePercent={interestRatePercent}
-        investmentTermMonths={investmentTermMonths}
-        interestPaid={interestPaid}
-      />,
+      <DepositCalculator />,
     );
 
     const finalBalanceElement = screen.getByTestId('final-balance');
