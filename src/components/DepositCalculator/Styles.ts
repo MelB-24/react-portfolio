159ee25c3f5151eb0bfe0731cf49b67b10d0f14c
Melissa Bykersma <melissa@cognativ.com.au>
2023-08-12 22:44:57 +1000

    Feat: add styling for basic calculator page

---
+++ src/components/DepositCalculator/Styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const DepositCalculatorContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Heading = styled.h1`
+  margin-top: 5rem;
+  margin-bottom: 3rem;
+`;
+
+export const CalcContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 20%;
+`;
