40a584d2fde64b0ffb8c1cf28cb9482087d6b3a8
Melissa Bykersma <melissa@cognativ.com.au>
2023-08-13 00:10:51 +1000

    Feat: add SubmitButton styling

--- src/components/DepositCalculator/Styles.ts
+++ src/components/DepositCalculator/Styles.ts
@@ -15,5 +15,25 @@ export const Heading = styled.h1`
 export const CalcContainer = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
+  align-items: center;
+`;
+
+export const SubmitButton = styled.button`
   width: 20%;
+  border: none;
+  background: none;
+  outline: none;
+  cursor: pointer;
+  margin-top: 0.5rem;
+  border: 2px solid #9BE368;
+  border-radius: 0.2rem;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1.3rem;
+
+  &:hover {
+    background: #9BE368;
+    color: white;
+    transform: scale(0.9);
+  }
 `;
