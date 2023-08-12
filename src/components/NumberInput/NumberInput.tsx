d383a3f47d6d6b1a42080b2cdc75985df3fd2588
Melissa Bykersma <melissa@cognativ.com.au>
2023-08-12 22:44:14 +1000

    Feat: add logic for NumberInput component

--- src/components/NumberInput/NumberInput.tsx
+++ src/components/NumberInput/NumberInput.tsx
@@ -1,13 +1,55 @@
 import React from 'react';
 
-const NumberInput: React.FC<NumberInputProps> = () => {
-  console.log('number input component');
+import {
+  NumberInputContainer, Input, InputLabel,
+} from './Styles';
+
+const NumberInput: React.FC<NumberInputProps> = ({ value, setValue, label }) => {
+  // TODO: on blur, wanted to format the string to currency
+  // const formatAsAUD = () => {
+  //   const validInput = /^(\d+(\.\d{0,2})?)?$/.test(value);
+
+  //   if (validInput) {
+  //     // Format the value as Australian dollars
+  //     const formattedValue = parseFloat(value || '0').toLocaleString('en-AU', {
+  //       style: 'currency',
+  //       currency: 'AUD',
+  //       minimumFractionDigits: 2,
+  //       maximumFractionDigits: 2,
+  //     });
+
+  //     setValue(formattedValue);
+  //   }
+  // };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+
+    const validInput = /^(\d+(\.\d{0,2})?)?$/.test(inputValue);
+
+    if (validInput) {
+      setValue(inputValue);
+    }
+  };
 
   return (
-    <>hi</>
+    <NumberInputContainer>
+      <InputLabel htmlFor="numberInput">{label}</InputLabel>
+      <Input
+        id="numberInput"
+        type="text"
+        value={value}
+        onChange={handleChange}
+        // onBlur={formatAsAUD}
+      />
+    </NumberInputContainer>
   );
 };
 
-interface NumberInputProps { }
+interface NumberInputProps {
+  value: string
+  setValue: (value: string) => void
+  label: string
+}
 
 export default NumberInput;
---
+++ src/components/NumberInput/Styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const NumberInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1rem
+`;
+
+export const InputLabel = styled.label`
+  margin-bottom: 0.3rem;
+`;
+
+export const Input = styled.input``;
