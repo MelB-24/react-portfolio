d581dcb626d8bbcbc64ef6beeb2d0d03188c498e
Melissa Bykersma <melissa@cognativ.com.au>
2023-08-12 19:31:17 +1000

    Project: add RadioGroup component files

---
+++ src/components/RadioGroup/RadioGroup.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RadioGroup: React.FC<RadioGroupProps> = () => {
+  console.log('RadioGroup component');
+
+  return (
+    <>hi</>
+  );
+};
+
+interface RadioGroupProps { }
+
+export default RadioGroup;
