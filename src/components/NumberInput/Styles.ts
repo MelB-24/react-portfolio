4b9bb88b4ff4cab2f2fab21cf9edf10c9f59eb7d
Melissa Bykersma <melissa@cognativ.com.au>
2023-08-13 00:10:37 +1000

    Fix: fix width and font-size styling on number input

--- src/components/NumberInput/Styles.ts
+++ src/components/NumberInput/Styles.ts
@@ -3,11 +3,13 @@ import styled from 'styled-components';
 export const NumberInputContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin-bottom: 1rem
+  margin-bottom: 1rem;
+  width: 30%;
 `;
 
 export const InputLabel = styled.label`
   margin-bottom: 0.3rem;
+  font-size: 1.1rem;
 `;
 
 export const Input = styled.input``;
