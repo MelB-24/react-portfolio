4262ba4240f6515c9e97d57cc9d67e03959c013d
Melissa Bykersma <melissa@cognativ.com.au>
2023-08-13 21:35:03 +1000

    Feat: update test to type in input, check for correct values and display correct final balance

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import DepositCalculator from './components/DepositCalculator';
 
-const startingAmount = 10000;
-const interestRatePercent = 1.1;
-const investmentTermMonths = 24;
-const interestPaid = 'month';
-const finalBalance = 10222;
+const startingAmount = '10000';
+const interestRatePercent = '2.7';
+const investmentTermMonths = '24';
+const interestPaidLabel = 'Quarterly';
+const finalBalance = '$10,553';
 
 describe('Term Deposit Calculator', () => {
   test('correctly calculates and displays the final balance', () => {
@@ -14,7 +14,23 @@ describe('Term Deposit Calculator', () => {
       <DepositCalculator />,
     );
 
+    const depositInput = screen.getByLabelText('Starting Deposit ($)');
+    fireEvent.change(depositInput, { target: { value: startingAmount } });
+    expect(depositInput).toHaveValue(startingAmount);
+
+    const interestRateInput = screen.getByLabelText('Interest Rate (% p.a.)');
+    fireEvent.change(interestRateInput, { target: { value: interestRatePercent } });
+    expect(interestRateInput).toHaveValue(interestRatePercent);
+
+    const investmentTermInput = screen.getByLabelText('Investment Term (months)');
+    fireEvent.change(investmentTermInput, { target: { value: investmentTermMonths } });
+    expect(investmentTermInput).toHaveValue(investmentTermMonths);
+
+    const interestPaidQuarterlyRadio = screen.getByLabelText(interestPaidLabel);
+    fireEvent.click(interestPaidQuarterlyRadio);
+    expect(interestPaidQuarterlyRadio).toBeChecked();
+
     const finalBalanceElement = screen.getByTestId('final-balance');
-    expect(finalBalanceElement).toHaveTextContent(finalBalance.toString());
+    expect(finalBalanceElement).toHaveTextContent(`Final Balance: ${finalBalance}`);
   });
 });
