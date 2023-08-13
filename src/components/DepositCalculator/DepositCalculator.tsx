8c6fa2cdd1e862cabd4c532644de37957ce1eedc
Melissa Bykersma <melissa@cognativ.com.au>
2023-08-13 20:21:16 +1000

    Feat: add inputs and display calculation

--- src/components/DepositCalculator/DepositCalculator.tsx
+++ src/components/DepositCalculator/DepositCalculator.tsx
@@ -1,23 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const DepositCalculator: React.FC<DepositCalculatorProps> = ({
-  startingAmount,
-  interestRatePercent,
-  investmentTermMonths,
-  interestPaid,
-}) => {
-  console.log('DepositCalculator component');
+import NumberInput from '../NumberInput';
+import RadioGroup from '../RadioGroup';
+
+import { CalcContainer, Heading, DepositCalculatorContainer } from './Styles';
+import { calculateCompoundInterest, calculateSimpleInterest } from '../../lib/utils';
+
+const DepositCalculator: React.FC<DepositCalculatorProps> = () => {
+  const [startingAmount, setStartingAmount] = useState('10000');
+  const [interestRatePercent, setInterestRatePercent] = useState('1.11');
+  const [investmentTermMonths, setInvestmentTermMonths] = useState('12');
+  const [interestPaidFreq, setInterestPaidFreq] = useState('month');
+
+  const radioOptions = [
+    {
+      label: 'Monthly',
+      value: 'month',
+    },
+    {
+      label: 'Quarterly',
+      value: 'quarter',
+    },
+    {
+      label: 'Annually',
+      value: 'annual',
+    },
+    {
+      label: 'At Maturity',
+      value: 'at-maturity',
+    },
+  ];
+
+  const finalBalance = () => {
+    let result = 0;
+    if (interestPaidFreq === 'at-maturity') {
+      result = calculateSimpleInterest({
+        initialDeposit: startingAmount,
+        interestRate: interestRatePercent,
+        investmentMonths: investmentTermMonths,
+      });
+    } else {
+      result = calculateCompoundInterest({
+        frequency: interestPaidFreq,
+        initialDeposit: startingAmount,
+        interestRate: interestRatePercent,
+        investmentMonths: investmentTermMonths,
+      });
+    }
+
+    const formattedResult = (result || 0).toLocaleString('en-AU', {
+      style: 'currency',
+      currency: 'AUD',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+
+    return formattedResult;
+  };
 
   return (
-    <>hi</>
+    <DepositCalculatorContainer>
+      <Heading>Term Deposit Calculator</Heading>
+      <CalcContainer>
+        <NumberInput value={startingAmount} setValue={setStartingAmount} label="Starting Deposit ($)" />
+        <NumberInput value={interestRatePercent} setValue={setInterestRatePercent} label="Interest Rate (% p.a.)" />
+        <NumberInput value={investmentTermMonths} setValue={setInvestmentTermMonths} label="Investment Term (months)" />
+        <RadioGroup value={interestPaidFreq} setValue={setInterestPaidFreq} label="Interest Paid" radioOptions={radioOptions} />
+      </CalcContainer>
+      <p>
+        {finalBalance()}
+      </p>
+    </DepositCalculatorContainer>
   );
 };
 
-interface DepositCalculatorProps {
-  startingAmount: number
-  interestRatePercent: number
-  investmentTermMonths: number
-  interestPaid: 'month' | 'quarter' | 'annual' | 'maturity'
-}
+interface DepositCalculatorProps { }
 
 export default DepositCalculator;
