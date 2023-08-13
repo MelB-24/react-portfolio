bb30e809c49340be6da21643457e994a456567f8
Melissa Bykersma <melissabykersma@gmail.com>
2023-08-13 22:31:43 +1000

    Chore: remove comments

--- src/components/NumberInput/NumberInput.tsx
+++ src/components/NumberInput/NumberInput.tsx
@@ -5,23 +5,6 @@ import {
 } from './Styles';
 
 const NumberInput: React.FC<NumberInputProps> = ({ value, setValue, label, htmlFor }) => {
-  // TODO: on blur, wanted to format the string to currency
-  // const formatAsAUD = () => {
-  //   const validInput = /^(\d+(\.\d{0,2})?)?$/.test(value);
-
-  //   if (validInput) {
-  //     // Format the value as Australian dollars
-  //     const formattedValue = parseFloat(value || '0').toLocaleString('en-AU', {
-  //       style: 'currency',
-  //       currency: 'AUD',
-  //       minimumFractionDigits: 2,
-  //       maximumFractionDigits: 2,
-  //     });
-
-  //     setValue(formattedValue);
-  //   }
-  // };
-
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value;
 
@@ -40,7 +23,6 @@ const NumberInput: React.FC<NumberInputProps> = ({ value, setValue, label, htmlF
         type="text"
         value={value}
         onChange={handleChange}
-        // onBlur={formatAsAUD}
       />
     </NumberInputContainer>
   );
