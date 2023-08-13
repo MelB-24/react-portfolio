920173d2cb9ec52ddf464604a5a0ce887c400799
Melissa Bykersma <melissa@cognativ.com.au>
2023-08-13 21:27:35 +1000

    Fix: update formatter to display as whole number

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -80,8 +80,8 @@ export const formatDollar = (result: number) => {
   const formattedResult = (result || 0).toLocaleString('en-AU', {
     style: 'currency',
     currency: 'AUD',
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
   });
 
   return formattedResult;
