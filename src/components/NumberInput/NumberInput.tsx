c5246050a2644952b732655b60bba6be0d01e854
Melissa Bykersma <melissa@cognativ.com.au>
2023-08-13 21:21:59 +1000

    Feat: add htmlFor prop for NumberInput

--- src/components/NumberInput/NumberInput.tsx
+++ src/components/NumberInput/NumberInput.tsx
@@ -4,7 +4,7 @@ import {
   NumberInputContainer, Input, InputLabel,
 } from './Styles';
 
-const NumberInput: React.FC<NumberInputProps> = ({ value, setValue, label }) => {
+const NumberInput: React.FC<NumberInputProps> = ({ value, setValue, label, htmlFor }) => {
   // TODO: on blur, wanted to format the string to currency
   // const formatAsAUD = () => {
   //   const validInput = /^(\d+(\.\d{0,2})?)?$/.test(value);
@@ -34,9 +34,9 @@ const NumberInput: React.FC<NumberInputProps> = ({ value, setValue, label }) => 
 
   return (
     <NumberInputContainer>
-      <InputLabel htmlFor="numberInput">{label}</InputLabel>
+      <InputLabel htmlFor={htmlFor}>{label}</InputLabel>
       <Input
-        id="numberInput"
+        id={htmlFor}
         type="text"
         value={value}
         onChange={handleChange}
@@ -50,6 +50,7 @@ interface NumberInputProps {
   value: string
   setValue: (value: string) => void
   label: string
+  htmlFor: string
 }
 
 export default NumberInput;
