9122bebbdac10dc31ef8d855b6f2e6924a81cfc1
Melissa Bykersma <melissa@cognativ.com.au>
2023-08-13 21:22:48 +1000

    Chore: wrap label around radio button

--- src/components/RadioGroup/RadioGroup.tsx
+++ src/components/RadioGroup/RadioGroup.tsx
@@ -10,15 +10,17 @@ const RadioGroup: React.FC<RadioGroupProps> = ({ value, setValue, label, radioOp
     <ButtonRow>
       {radioOptions.map((option) => (
         <RadioButtonWrapper key={option.value}>
-          <ButtonLabel htmlFor={option.value} checked={option.value === value}>{option.label}</ButtonLabel>
-          <RadioButton
-            id={option.value}
-            type="radio"
-            name="radio-group"
-            value={option.value}
-            checked={option.value === value}
-            onChange={(e) => { setValue(e.target.value); }}
-          />
+          <ButtonLabel htmlFor={option.value} checked={option.value === value}>
+            <RadioButton
+              id={option.value}
+              type="radio"
+              name="radio-group"
+              value={option.value}
+              checked={option.value === value}
+              onChange={(e) => { setValue(e.target.value); }}
+            />
+            {option.label}
+          </ButtonLabel>
         </RadioButtonWrapper>
       ))}
     </ButtonRow>
